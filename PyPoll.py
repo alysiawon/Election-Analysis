7461c0f66757731b543ae3f7c27df5d49a6013ac
alysiawon <won.alysia@gmail.com>
2021-05-07 08:48:43 -0400

    Update

---
+++ PyPoll.py
@@ -0,0 +1,14 @@
+# 1. The data we need to retrieve
+
+# Import the datetime class from the datetime module.
+import datetime
+# Use the now() attribute on the datetime class to get the present time.
+now = datetime.datetime.now()
+# Print the present time.
+print("The time right now is ", now)
+
+# 2. The total number of votes casted
+# 3. A complete list of candidates who received votes
+# 4. The percentage of votes each candidate won
+# 5. The total number of votes each candidate won
+# 6. The winnier of the election based on popular vote
--- Python_Practice.py
+++ Python_Practice.py
@@ -54,15 +54,181 @@
 # voting_data["county"].append("Arapahoe")
 # print(voting_data)5
 
-# Decision Statements: If Statement
-counties = ["Arapahoe","Denver","Jefferson"]
-if counties[1] == 'Denver':
-    print(counties[1])
+# # Decision Statements: If Statement
+# counties = ["Arapahoe","Denver","Jefferson"]
+# if counties[1] == 'Denver':
+#     print(counties[1])
 
-# Decision Statements: Else-If Statement
-temperature = int(input("What is the temperature outside? "))
+# # Decision Statements: If-Else Statement
+# temperature = int(input("What is the temperature outside? "))
 
-if temperature > 80:
-    print("Turn on the AC.")
-else:
-    print("Open the windows.")
+# if temperature > 80:
+#     print("Turn on the AC.")
+# else:
+#     print("Open the windows.")
+
+# # Decision Statements: Nested If-Else Statements
+#     #What is the score?
+# score = int(input("What is your test score? "))
+
+#     # Determine the grade.
+# if score >= 90:
+#     print('Your grade is an A.')
+# else:
+#     if score >= 80:
+#         print('Your grade is a B.')
+#     else:
+#         if score >= 70:
+#             print('Your grade is a C.')
+#         else:
+#             if score >= 60:
+#                 print('Your grade is a D.')
+#             else:
+#                 print('Your grade is an F.')
+
+# # Decision Statements: elif Statements
+#     # What is the score?
+# score = int(input("What is your test score? "))
+
+#     # Determine the grade.
+# if score >= 90:
+#     print('Your grade is an A.')
+# elif score >= 80:
+#     print('Your grade is a B.')
+# elif score >= 70:
+#     print('Your grade is a C.')
+# elif score >= 60:
+#     print('Your grade is a D.')
+# else:
+#     print('Your grade is an F.')
+
+# # Membership Operators
+# counties = ["Arapahoe","Denver","Jefferson"]
+# if "El Paso" in counties:
+#     print("El Paso is in the list of counties.")
+# else:
+#     print("El Paso is not the list of counties.")
+
+# #Logic Operators
+# counties = ["Arapahoe","Denver","Jefferson"]
+
+#     #and
+# if "Arapahoe" in counties and "El Paso" in counties:
+#     print("Arapahoe and El Paso are in the list of counties.")
+# else:
+#     print("Arapahoe or El Paso is not in the list of counties.")
+
+#     #or
+# if "Arapahoe" in counties or "El Paso" in counties:
+#     print("Arapahoe or El Paso is in the list of counties.")
+# else:
+#     print("Arapahoe and El Paso are not in the list of counties.")
+
+# # While Loops
+# x = 0
+# while x <= 5:
+#     print(x)
+#     x = x + 1
+
+# # Iterate Through Lists and Tuples
+# counties = ["Arapahoe","Denver","Jefferson"]
+# for county in counties:
+#     print(county)
+
+# numbers = [0, 1, 2, 3, 4]
+# for num in numbers:
+#     print(num)
+
+# for num in range(5):
+#     print(num)
+
+# for i in range(len(counties)):
+#     print(counties[i])
+
+# # Iterate Through A Dictionary
+# counties_dict = {}
+# counties_dict["Arapahoe"] = 422829
+# counties_dict["Denver"] = 463353
+# counties_dict["Jefferson"] = 432438
+
+# for county in counties_dict:
+#     print(county)
+
+#     # Get Keys in Dictionary
+# for county in counties_dict.keys():
+#     print(county)
+
+#     # Get Values in Dictionary
+# for voters in counties_dict.values():
+#     print(voters)
+
+# for county in counties_dict:
+#     print(counties_dict[county])
+
+# for county in counties_dict:
+#     print(counties_dict.get(county))
+
+    # Get Key-Value Pairs in Dictionary
+# for county, voters in counties_dict.items():
+#     print(county, voters)
+
+# # Iterate Through A List of Dictionaries
+# voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+#                 {"county":"Denver", "registered_voters": 463353},
+#                 {"county":"Jefferson", "registered_voters": 432438}]
+
+# for county_dict in voting_data:
+#     print(county_dict)
+
+#     # Print Using range()
+# for i in range(len(voting_data)): 
+#     print(voting_data[i])
+
+# # Get Values from a List of Dictionaries
+# for county_dict in voting_data:
+#     for value in county_dict.values():
+#         print(value)
+        
+# # Get Only Voter Values
+# for county_dict in voting_data:
+#      print(county_dict['registered_voters'])
+
+# # Get Only County Values
+# for county_dict in voting_data:
+#     print(county_dict['county'])
+
+# # Print()
+# my_votes = int(input("How many votes did you get in the election? "))
+# total_votes = int(input("What is the total votes in the election? "))
+# percentage_votes = (my_votes / total_votes) * 100
+# print("I received " + str(percentage_votes)+"% of the total votes.")
+
+# # F-Strings Print
+# my_votes = int(input("How many votes did you get in the election? "))
+# total_votes = int(input("What is the total votes in the election? "))
+# print(f"I received {my_votes / total_votes * 100}% of the total votes.")
+
+# # Using F-Strings With Dictionary
+#     # Print
+# counties_dict = {"Arapahoe": 369237, "Denver":413229, "Jefferson": 390222}
+# for county, voters in counties_dict.items():
+#     print(county + " county has " + str(voters) + " registered voters.")
+
+#     # F-String
+# for county, voters in counties_dict.items():
+#     print(f"{county} county has {voters} registered voters.")
+
+# # Multiline F-Strings
+# candidate_votes = int(input("How many votes did the candidate get in the election? "))
+# total_votes = int(input("What is the total number of votes in the election? "))
+# message_to_candidate = (
+#     f"You received {candidate_votes} number of votes. "
+#     f"The total number of votes in the election was {total_votes}. "
+#     f"You received {candidate_votes / total_votes * 100}% of the total votes.")
+# print(message_to_candidate)
+
+# # Format Floating-Point Decimals
+# message_to_candidate = (
+#     f"You received {candidate_votes:,} number of votes. "
+#     f"The total number of votes in the election was {total_votes:,}. "
+#     f"You received {candidate_votes / total_votes * 100:.2f}% of the total votes.")
